fb4c342512f2cf8168dad9e318f640faf2bea53e
percyten <percyt@esna.com>
2017-02-10 20:25:47 -0500

    Added hover effect

--- src/components/homePage.js
+++ src/components/homePage.js
@@ -6,6 +6,7 @@ export default class HomePage extends Component{
     this.state={
       imageList: [],
       selectedImage:{},
+      display:[],
     }
   }
   componentWillMount(){
@@ -20,16 +21,34 @@ export default class HomePage extends Component{
       }
       else{
         console.log(response.data.photos)
-        _this.setState({imageList:response.data.photos})
+        _this.setState({imageList:response.data.photos}, ()=>{
+          var tmp = new Array(_this.state.imageList.length)
+          tmp.fill(0)
+          _this.setState({display:tmp})
+        })
+
       }
     })
+  }
+  onMouseOver(i){
+    var tmp = new Array(this.state.display.length)
+    tmp.fill(0)
+    tmp[i] = 1
+    this.setState({display: tmp})
+
+  }
+  onMouseLeave(){
+    var tmp = new Array(this.state.display.length)
+    tmp.fill(0)
+    this.setState({display: tmp})
   }
   renderImages(){
     return(
       this.state.imageList.map((image, i)=>(
-          <div key={i} className='animated zoomIn'
+          <div onMouseLeave= {this.onMouseLeave.bind(this)} onMouseOver= {this.onMouseOver.bind(this,i)} key={i} className='animated zoomIn images'
             style = {{
-              width: '32%',
+              width:'32%',
+              position:'relative',
               height:window.innerWidth*0.3*0.8,
               marginTop:window.innerWidth*.005,
               marginLeft: '1%',
@@ -41,7 +60,16 @@ export default class HomePage extends Component{
               backgroundPosition: 'center',
               display:'inline-block',
             }}>
-
+            <div style={{
+              paddingLeft: '5px',
+              height: '20%',
+              position:'relative',
+              backgroundColor: '#000',
+              opacity: this.state.display[i],
+            }}>
+              <p style={{color: '#fff', marginLeft: '10px', paddingTop:'10px',fontFamily: 'Avenir', fontSize: 13}}>{image.name}</p>
+              <p style={{color: '#fff', position:'absolute', bottom:10, right: 10, fontFamily: 'Avenir', fontSize: 13}}>- {image.user.fullname}</p>
+            </div>
           </div>
         )
       )
@@ -62,6 +90,7 @@ export default class HomePage extends Component{
         backgroundColor: '#f9f9f9',
         height: '500px',
       },
+
       titleDiv:{
         marginTop: "20px",
         marginBottom: '1px',
